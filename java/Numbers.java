87f679e15e7f72252b1fbd7d6dacc114ace1ef62
Vaibhav Satish Thete <168334198+Vaibhavthete12@users.noreply.github.com>
2026-06-11 10:49:52 +0530

    Update Numbers.java

--- java/Numbers.java
+++ java/Numbers.java
@@ -2,14 +2,14 @@ public class Numbers {
 
     public static void main(String[] args) {
 
-        int[] numbers = {10,25,5,18,30,12,45,7,22,15};
+        int[] numbers = {10,25,5,18,30,12,45,7,22,15,
+                         50,3,28,17,60,9,35,41,11,26};
 
         int highest = numbers[0];
         int lowest = numbers[0];
         int sum = 0;
 
         for(int n : numbers) {
-
             if(n > highest)
                 highest = n;
 
